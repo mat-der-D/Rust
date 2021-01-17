a436acd8f91c30a6ec5768e66349ff42f97a9870
mat_der_D <pmahthyemsatics.14or0@gmail.com>
2021-01-17 19:10:32 +0900

    added first_course

---
+++ first_course/guessing_game/target/debug/deps/guessing_game-1abe535afe4e42fa.d
@@ -0,0 +1,5 @@
+/home/toshio/Documents/sandbox/rust/guessing_game/target/debug/deps/guessing_game-1abe535afe4e42fa: src/main.rs
+
+/home/toshio/Documents/sandbox/rust/guessing_game/target/debug/deps/guessing_game-1abe535afe4e42fa.d: src/main.rs
+
+src/main.rs:
---
+++ first_course/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/toshio/Documents/sandbox/rust/guessing_game/target/debug/guessing_game: /home/toshio/Documents/sandbox/rust/guessing_game/src/main.rs
